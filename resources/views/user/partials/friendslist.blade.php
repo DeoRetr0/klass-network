50f4b6ea441259089ec7272b9db7fcef6dce216e
Josué de Paulo Viana <josuep.viana@gmail.com>
2020-06-25 00:00:22 -0300

    Upload de Fotos

--- resources/views/user/partials/friendslist.blade.php
+++ resources/views/user/partials/friendslist.blade.php
@@ -1,6 +1,6 @@
 <div class="media col" style="margin-bottom: 10px">
     <a href="{{route('profile.Perfil', ['username' =>$user->username])}}" class="pull-left">
-        <img class="media-object" STYLE="width: 100px" src="{{Storage::url("uploads/avatars/{$user->avatar}")}}" alt="{{$user->getName()}}">
+        <img class="media-object" STYLE="width: 100px" src="{{URL::asset("uploads/avatars/{$user->avatar}")}}" alt="{{$user->getName()}}">
     </a>
     <div class="media-body">
         <h4 class="media-heading"><a href="{{route('profile.Perfil', ['username' =>$user->username])}}">{{$user->getName()}}</a></h4>
--- resources/views/user/partials/userbasic.blade.php
+++ resources/views/user/partials/userbasic.blade.php
@@ -1,4 +1,4 @@
     <a href="{{route('profile.Perfil', ['username' =>$user->username])}}" class="pull-left">
-        <img class="media-object"  style="margin-bottom: 7px; margin-left: -7px; padding: 2px; width: 63px" src="{{Storage::url("uploads/avatars/{$user->avatar}")}}" alt="{{$user->getName()}}">
+        <img class="media-object"  style="margin-bottom: 7px; margin-left: -7px; padding: 2px; width: 63px" src="{{URL::asset("uploads/avatars/{$user->avatar}")}}" alt="{{$user->getName()}}">
     </a>
 
