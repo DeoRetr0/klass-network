7297b42a35d02745b10044b040b48495edb32955
Josué de Paulo Viana <josuep.viana@gmail.com>
2020-06-24 23:54:06 -0300

    Teste de Upload 1

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -31,7 +31,7 @@ class ProfileController extends Controller
         if($request->hasFile('avatar')){
             $avatar = $request->file('avatar');
             $filename = time().'.'.$avatar->getClientOriginalExtension();
-            Image::make($avatar)->resize(300, 300)->save(public_path('storage/uploads/avatars/'.$filename));
+            Image::make($avatar)->resize(300, 300)->save('uploads/avatars/'.$filename);
 
             $user = Auth::user();
             $user->avatar = $filename;
