08f56db56ddf6e8975baa69f99ee8ae20d7eb9aa
Josué de Paulo Viana <josuep.viana@gmail.com>
2021-01-09 23:52:57 -0300

    novo bg - pgsql

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -48,7 +48,7 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'ec2-34-237-166-54.compute-1.amazonaws.com'),
-            'port' => env('DB_PORT', '5432'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'd6f4d50ceul7sh'),
             'username' => env('DB_USERNAME', 'xidwdipcotjvjo'),
             'password' => env('DB_PASSWORD', '9d3f6e682d1969619a347adc67f4ee10860ac4c9637934a1d4a26db86c91aef9'),
@@ -66,12 +66,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('postgres://xidwdipcotjvjo:9d3f6e682d1969619a347adc67f4ee10860ac4c9637934a1d4a26db86c91aef9@ec2-34-237-166-54.compute-1.amazonaws.com:5432/d6f4d50ceul7sh'),
+            'host' => env('DB_HOST', 'ec2-34-237-166-54.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd6f4d50ceul7sh'),
+            'username' => env('DB_USERNAME', 'xidwdipcotjvjo'),
+            'password' => env('DB_PASSWORD', '9d3f6e682d1969619a347adc67f4ee10860ac4c9637934a1d4a26db86c91aef9'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
